a2a5787d5e08394eb686b3800a94c646f9e12c08
g109osan <84487906+g109osan@users.noreply.github.com>
2021-07-09 00:45:07 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>//sqrtを利用するために必要なヘッダーファイル
-
+#include <stdlib.h>//数値を変換するために必要なヘッダーファイル
 int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
 {
   int y;
